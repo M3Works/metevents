c494c95f6a44b84e8edfe899ae34bf742931667c
micah johnson <micah.johnson150@gmail.com>
2023-04-25 13:14:54 -0600

    Added workflow.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'metloom>=0.3.5,<1.0.0',
+    'metevents>=0.3.5,<1.0.0',
 ]
 
 test_requirements = ['pytest>=3', ]
