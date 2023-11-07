edd8aa8f24e56cab58a708b600d59eb1172d33fb
YangKehan <yangkehan@YangKehans-MacBook-Pro.local>
2023-11-07 10:18:42 -0500

    reformat files

--- tests/test_events.py
+++ tests/test_events.py
@@ -115,6 +115,8 @@ class TestOutlierEvents:
 
         outlier_storms.find()
         tolerance = 1e-10
-        assert outlier_storms.outliers.values.tolist() == \
-                pytest.approx(outliers, rel = tolerance, abs = tolerance)
+        assert (
+            outlier_storms.outliers.values.tolist()
+            == pytest.approx(outliers, rel = tolerance, abs = tolerance)
+        )
 
